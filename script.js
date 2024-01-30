3c2ac68f850baa7f92d2b3854b0f54161d4e44a0
Ruban Dzmitry <ruban251219872023@gmail.com>
2024-01-30 12:01:42 +0300

    loop finish

--- script.js
+++ script.js
@@ -31,7 +31,13 @@ while (remainingLetters > 0) {
     } else if (guess.length !== 1) {
         alert('Пожалуйста введите только одну букву!');
     } else {
-        //Состояние игры обновляется...
+        //Обновление состояние игры
+        for (var j = 0; j < word.length; j++) {
+            if (word[j] === guess) {
+                answerArray[j] = guess;
+                remainingLetters--;
+            }
+        }
     }
 }
 
